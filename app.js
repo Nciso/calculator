61372bfdd7436f94f8e9bfd2d77cf34da00feed1
nciso <encisoenrique@gmail.com>
2020-08-20 16:11:38 -0500

    Add better sum and handling cases when = is pressed

--- app.js
+++ app.js
@@ -16,57 +16,75 @@ const render = () => {
   document.querySelector('[data-binding="display"]').innerHTML = state.display;
 };
 
-const operands = [0, 0]
-
 const state = createState({
-  display: "0.00", // creates initial state
-  buffer: 0,
-  clearDisplay: true,
-  operator: null,
-  lastOperator: '+'
+  display: "0.00", // displays initial state in UI
+  clearDisplay: true, // check if need to rewrite display, keep last value on operations
+  lastOperator: "+",
+  operands: [0, 0],
 });
-// setup of display
+
+// setup of display on the ui
 document.querySelector('[data-binding="display"]').innerHTML = state.display;
 
+//events for blue buttons (numbers)
 document
   .getElementsByClassName("numbers-tiles")[0]
   .addEventListener("click", function (event) {
+    // TODO
+    if (event.target.innerText == "+/-") {
+      return;
+    }
+    // TODO
+    if (event.target.innerText == "%") {
+      return;
+    }
+    // RETURN TO INITIAL STATE
+    if (event.target.innerText == "C") {
+      state.display = "0.00";
+      state.clearDisplay = true;
+      lastOperator = "+";
+      return;
+    }
+
+    //Regular operation of the display
     if (state.clearDisplay) {
-      state.display = event.target.innerHTML;
+      state.display = event.target.innerText;
       state.clearDisplay = false;
     } else {
-      state.display += event.target.innerText; 
+      state.display += event.target.innerText;
     }
   });
 
+// events for orange buttons (operators)
 document
   .getElementsByClassName("operator-tiles")[0]
   .addEventListener("click", function (event) {
-      state.operator = event.target.innerText
-      operands[1] = state.display
-      state.clearDisplay =  true
-      for(let i=0; i<operands.length; i++) {
-        operands[i] = parseFloat(operands[i])
-      }
-      
-      if(state.operator == '+'){
-        operands[0] = sum(operands[0], operands[1])
-        operands[1] = 0
-        
-      }
-      if(state.operator == '='){
-        if(state.lastOperator == '+'){
-          operands[0] = sum(operands[0], operands[1])
-          operands[1] = 0
-        }
-      }
-      state.display = parseFloat(operands[0]).toFixed(2)
-      state.lastOperator = state.operator
+    state.operands[1] = state.display;
+    state.clearDisplay = true;
+    // convert operators into floats
+    for (let i = 0; i < state.operands.length; i++) {
+      state.operands[i] = parseFloat(state.operands[i]);
+    }
+    evaluate(state.operands, state.lastOperator);
+    if (event.target.innerText == "=") {
+      evaluate(state.operands, state.lastOperator);
+    }
+    state.lastOperator = event.target.innerText;
+    state.display = parseFloat(state.operands[0]).toFixed(2);
   });
 
+function evaluate(operands, lastOperator) {
+  const [a, b] = operands;
+  //  implement sum
+  if (lastOperator == "+") {
+    operands[0] = sum(a, b);
+    operands[1] = 0;
+  }
+  //implement if statements here
+}
+
+// implement functions below
 
 function sum(a, b) {
   return a + b;
 }
-
-// implement functions below
